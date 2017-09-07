9de4bcb8564341ba2523f8e9e90fb18a3fb11da7
Daniel Hart <DanielHartUK@users.noreply.github.com>
2017-09-07 16:48:27 +0100

    Added favicon

--- php/head.php
+++ php/head.php
@@ -4,5 +4,5 @@
 <link rel="stylesheet" href="assets/webfonts/stylesheet.css">
 <meta name="viewport" content="initial-scale=1.0">
 <meta name="description" content="">
-<link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
-<link rel="icon" href="assets/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" type="image/png" href="assets/favicon.png"/>
+<link rel="shortcut icon" type="image/png" href="assets/favicon.png"/>
