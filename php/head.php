6758463b980b82fdc1b313352d39e521217e53a6
Daniel Hart <mail@danielhart.co.uk>
2019-07-07 18:16:21 +0100

    Add Laravel Lumen

--- php/head.php
+++
@@ -1,6 +0,0 @@
-<meta charset="UTF-8">
-<link rel="stylesheet" href="css/normalize.css">
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="assets/webfonts/stylesheet.css">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="shortcut icon" type="image/png" href="assets/favicon.png"/>
